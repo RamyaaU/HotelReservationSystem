6b8403c8afcc37b1f476c8dc4413ba3e4a65e931
Ramya.U <ramyayadav2016@gmail.com>
2020-11-11 22:43:15 +0530

    [Ramya] Add. Added welcome message on master branch

--- HotelReservationSystem/Program.cs
+++ HotelReservationSystem/Program.cs
@@ -4,9 +4,13 @@ namespace HotelReservationSystem
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Welcoem to Hotel Reservation System!");
+            Console.WriteLine("Hello, Welcome to Hotel Reservation System!");
         }
     }
 }
