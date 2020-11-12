0892012ab998bb4e4f7cc5d737eed2d148fc7005
Ramya.U <ramyayadav2016@gmail.com>
2020-11-12 21:52:02 +0530

    [Ramya] Add . Added method to find the cheapest best rated hotel

---
+++ HotelReservationSystem/HotelReservationException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystem
+{
+    public class HotelReservationException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_HOTEL_TYPE,
+            INVALID_DATE
+        }
+
+        /// <summary>
+        /// The type
+        /// </summary>
+        ExceptionType type;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HotelReservationException"/> class.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="message">The message.</param>
+        public HotelReservationException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
