4cf5b8f951b7b5a7f1fe18daeb7d19a2f7b19888
Ramya.U <ramyayadav2016@gmail.com>
2020-11-12 18:38:50 +0530

    [Ramya] Add . Added method to find cheapest hotel based on weekdays and weekends

---
+++ HotelReservationSystem/HotelService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystem
+{
+    public class HotelService
+    {
+        public HotelType FindCheapestHotel(string startDate, string endDate)
+        {
+            //initialization
+            Hotel LakeWood = new Hotel(HotelType.LAKEWOOD);
+            Hotel BridgeWood = new Hotel(HotelType.BRIDGEWOOD);
+            Hotel RidgeWood = new Hotel(HotelType.RIDGEWOOD);
+
+            //calculating rate of each hotel between the given dates
+            double LakeWoodRate = LakeWood.FindTotalCost(startDate, endDate);
+            double BridgeWoodRate = BridgeWood.FindTotalCost(startDate, endDate);
+            double RidgeWoodRate = RidgeWood.FindTotalCost(startDate, endDate);
+
+            //returns the smaller value between two rates
+            double MinRate = Math.Min(LakeWoodRate, Math.Min(BridgeWoodRate, RidgeWoodRate));
+            if (MinRate == LakeWoodRate)
+                return HotelType.LAKEWOOD;
+            if (MinRate == BridgeWoodRate)
+                return HotelType.BRIDGEWOOD;
+            return HotelType.RIDGEWOOD;
+        }
+    }
+}
