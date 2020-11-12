9c7c6ff5e3cac13b431d078c2b91e5a5254ac79e
Ramya.U <ramyayadav2016@gmail.com>
2020-11-12 22:45:23 +0530

    [Ramya] Add . Added method to find the best rated hotel for a given sata range

---
+++ HotelReservationSystem/HotelService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystem
+{
+    public class HotelService
+    {
+        public HotelType FindCheapestHotel(string startDate, string endDate)
+        {
+            //initialization
+            Hotel LakeWood = new Hotel(HotelType.LAKEWOOD);
+            Hotel BridgeWood = new Hotel(HotelType.BRIDGEWOOD);
+            Hotel RidgeWood = new Hotel(HotelType.RIDGEWOOD);
+
+            //calculating rate of each hotel between the given dates
+            double LakeWoodRate = LakeWood.FindTotalCost(startDate, endDate);
+            double BridgeWoodRate = BridgeWood.FindTotalCost(startDate, endDate);
+            double RidgeWoodRate = RidgeWood.FindTotalCost(startDate, endDate);
+
+            //returns the smaller value between two rates
+            double MinRate = Math.Min(LakeWoodRate, Math.Min(BridgeWoodRate, RidgeWoodRate));
+            if (MinRate == LakeWoodRate && MinRate == BridgeWoodRate && MinRate == RidgeWoodRate)
+                return HotelType.RIDGEWOOD;
+            if (MinRate == LakeWoodRate && MinRate == BridgeWoodRate)
+                return HotelType.BRIDGEWOOD;
+            if (MinRate == BridgeWoodRate && MinRate == RidgeWoodRate)
+                return HotelType.RIDGEWOOD;
+            if (MinRate == LakeWoodRate && MinRate == BridgeWoodRate)
+                return HotelType.RIDGEWOOD;
+            if (MinRate == LakeWoodRate)
+                return HotelType.LAKEWOOD;
+            if (MinRate == BridgeWoodRate)
+                return HotelType.BRIDGEWOOD;
+            return HotelType.RIDGEWOOD;
+        }
+
+        /// <summary>
+        /// Finds the best rated hotel.
+        /// </summary>
+        /// <param name="startDate">The start date.</param>
+        /// <param name="endDate">The end date.</param>
+        /// <returns></returns>
+        public HotelType FindBestRatedHotel(string startDate, string endDate)
+        {
+            //initialization
+            Hotel RidgeWood = new Hotel(HotelType.RIDGEWOOD);
+            Hotel BridgeWood = new Hotel(HotelType.BRIDGEWOOD);
+            Hotel LakeWood = new Hotel(HotelType.LAKEWOOD);
+            //returns the larger vlaue of two different hotel rates
+            double MaxRating = Math.Max(RidgeWood.RATING, Math.Max(BridgeWood.RATING, LakeWood.RATING));
+            if (MaxRating == RidgeWood.RATING)
+                return HotelType.RIDGEWOOD;
+            if (MaxRating == BridgeWood.RATING)
+                return HotelType.BRIDGEWOOD;
+            else
+                return HotelType.LAKEWOOD;
+        }
+    }
+}
